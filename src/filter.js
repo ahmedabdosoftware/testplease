be8aa3e46d42b50c2ec5de37148d081eadd471ed
ahmed <ahmedabdosoftware@gmail.com>
2024-11-17 16:34:32 +0200

    Updates

--- src/filter.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue";
-Vue.filter("sub", function (text) {
-  return text.substring(0, 35) + "...";
-});
--- src/plugins/fontawesome.js
+++
@@ -1,9 +0,0 @@
-// src/plugins/fontawesome.js
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUndo,faUser, faLock, faPrint, faHouse, faStore, faBriefcase, faSignOutAlt, faArrowRight, faFolder, faComments, faAdjust, faUserCircle, faChevronRight, faChevronLeft, faChevronDown,faChevronUp, faMoon, faSun, faTools, faUserTie, faProjectDiagram, faImage, faStickyNote, faBoxOpen, faTags, faUserSlash, faExclamationCircle, faEye, faEdit, faTrash, faFileInvoice,faFileInvoiceDollar, faShoppingCart, faClipboardList, faDollarSign, faCashRegister,faBalanceScale,faWarehouse, faBoxes, faFilePdf,faExchangeAlt,faBell,faTasks  } from '@fortawesome/free-solid-svg-icons'
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faUndo,faUser, faLock, faPrint, faHouse, faStore, faBriefcase, faSignOutAlt, faArrowRight, faFolder, faComments, faAdjust, faUserCircle, faChevronRight, faChevronLeft, faChevronDown,faChevronUp, faMoon, faSun, faTools, faUserTie, faProjectDiagram, faImage, faStickyNote, faBoxOpen, faTags, faUserSlash, faExclamationCircle, faEye, faEdit, faTrash, faFileInvoice,faWhatsapp, faFileInvoiceDollar, faShoppingCart, faClipboardList, faDollarSign, faCashRegister,faBalanceScale,faWarehouse, faBoxes, faFilePdf,faExchangeAlt,faBell,faTasks )
-
-export default FontAwesomeIcon;
